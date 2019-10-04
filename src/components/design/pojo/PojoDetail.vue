957a9b55b9bec7ba729a9cd400af5f36fe430750
capser <solitary@163.com>
2019-10-04 20:55:35 +0800

    构建pojo的相关界面

--- src/components/design/pojo/PojoDetail.vue
+++ src/components/design/pojo/PojoDetail.vue
@@ -1,57 +1,86 @@
 <template>
-    <el-container>
-        <el-header style="align-items: flex-start;font-size: x-large">
-            <span>新建</span>
-        </el-header>
-        <el-main>
-            <el-divider content-position="left">基本信息</el-divider>
-            <el-form v-model="pojoInfo" :inline="true">
-                <el-form-item label="名称">
-                    <el-input v-model="pojoInfo.name"></el-input>
-                </el-form-item>
-                <el-form-item label="类型">
-                    <el-select v-model="pojoInfo.type" value="">
-                        <el-option v-for="(item,index) in typeList" :key="index" :label="item.name"
-                                   :value="item.value"></el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="描述">
-                    <el-input v-model="pojoInfo.desc"></el-input>
-                </el-form-item>
-            </el-form>
-            <el-divider content-position="left">属性信息</el-divider>
-            <el-table
-                    border
-                    :data="testTableData"
-            >
-                <el-table-column
-                        prop="name"
-                        label="名称"
+    <div>
+        <el-container>
+            <el-header style="align-items: flex-start;font-size: x-large">
+                <span>新建</span>
+            </el-header>
+            <el-main>
+                <el-divider content-position="left">基本信息</el-divider>
+                <el-form v-model="pojoInfo" :inline="true">
+                    <el-form-item label="名称">
+                        <el-input v-model="pojoInfo.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="类型">
+                        <el-select v-model="pojoInfo.type" value="">
+                            <el-option v-for="(item,index) in typeList" :key="index" :label="item.name"
+                                       :value="item.value"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="描述">
+                        <el-input v-model="pojoInfo.desc"></el-input>
+                    </el-form-item>
+                </el-form>
+                <el-divider content-position="left">属性信息</el-divider>
+                <el-button></el-button>
+                <el-table
+                        border
+                        :data="testTableData"
                 >
-                </el-table-column>
-                <el-table-column
-                        prop="type"
-                        label="类型"
-                ></el-table-column>
-                <el-table-column
-                        prop="desc"
-                        label="描述"
-                ></el-table-column>
-                <el-table-column
-                        label="操作"
-                >
-                    <template slot-scope="scope">
-                        <el-button type="primary">查看规则</el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-        </el-main>
-    </el-container>
+                    <el-table-column
+                            prop="name"
+                            label="名称"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                            prop="type"
+                            label="类型"
+                    ></el-table-column>
+                    <el-table-column
+                            prop="desc"
+                            label="描述"
+                    ></el-table-column>
+                    <el-table-column
+                            label="操作"
+                    >
+                        <template slot-scope="scope">
+                            <el-button type="primary" @click="showRule(scope.row)">查看规则</el-button>
+                            <el-button type="danger" @click="deleteAttr(scope.row)">删除属性</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-main>
+        </el-container>
+        <el-dialog
+                title="属性限制"
+                :visible.sync="rulePanel"
+        >
+            <PojoRule :attr="curAttr"/>
+        </el-dialog>
+    </div>
+
 </template>
 
 <script>
+    import PojoRule from "@/components/design/pojo/PojoRule";
+
     export default {
         name: "PojoDetail",
+        components: {
+            PojoRule
+        },
+        methods: {
+            showRule: function (row) {
+            debugger;
+                this.curAttr = row;
+                this.rulePanel = true;
+            },
+            deleteAttr(attr) {
+                let index = this.testTableData.indexOf(attr);
+                if (index !== -1) {
+                    this.testTableData.slice(index, 1)
+                }
+            },
+        },
         data() {
             return {
                 pojoInfo: {
@@ -60,6 +89,7 @@
                     desc: ""
                 },
                 typeList: this.$store.getters.pojoTypeList,
+                rulePanel: false,
                 testTableData: [
                     {
                         name: "test",
@@ -69,10 +99,11 @@
                             pattern: "",
                             max: "",
                             min: "",
-                            notNull: false
+                            notNull: 0
                         }
                     }
-                ]
+                ],
+                curAttr: {}
             }
         },
     }
---
+++ src/components/design/pojo/PojoRule.vue
@@ -0,0 +1,52 @@
+<template>
+    <el-container>
+        <el-main>
+            <el-form :model="tmpRule" :label-position="labelPosition" style="width:300px;padding: 10px">
+                <el-form-item label="正则表达式" style="">
+                    <el-input v-model="tmpRule.pattern" style="width: 100px"></el-input>
+                </el-form-item>
+                <el-form-item label="是否为空">
+                    <el-radio-group v-model="tmpRule.notNull" style="width: 200px">
+                        <el-radio :label="0">可空</el-radio>
+                        <el-radio :label="1">非空</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="最大值">
+                    <el-input v-model="tmpRule.max" style="width: 100px"></el-input>
+                </el-form-item>
+                <el-form-item label="最小值">
+                    <el-input v-model="tmpRule.min" style="width: 100px;"></el-input>
+                </el-form-item>
+            </el-form>
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "PojoRule",
+        props: {
+            submit: Function,
+            attr:Object
+        },
+        methods: {
+            _submit: function () {
+                if (this.submit instanceof Function) {
+                    this.submit();
+                }
+                console.log(this.tmpRule)
+            }
+        },
+        data() {
+            return {
+                labelPosition:"right",
+                tmpRule: this.attr.rule
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
